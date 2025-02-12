82b4eb1c10153ff91222bbe0f2a6e32da4e2588b
hashim79 <130667593+hashim79@users.noreply.github.com>
2025-02-12 22:47:19 +0500

    Update testbench.sv

--- Lab_2/Testbench/testbench.sv
+++ Lab_2/Testbench/testbench.sv
@@ -1,13 +1,10 @@
 module testbench;
-
 logic a1;
 logic b1;
 logic c1;
 logic y1;
 logic x1; 
-
 localparam period = 10;
-
 lab_2 UUT(
     .a(a1),  
     .b(b1),  
@@ -15,45 +12,30 @@ lab_2 UUT(
     .y(y1),
     .x(x1)  
 );
-
-
 initial
 begin
- 
     a1 = 0; b1 = 0; c1 = 0;
     #period; 
-    
     a1 = 0; b1 = 0; c1 = 1;
     #period;
-    
     a1 = 0; b1 = 1; c1 = 0;
     #period;
-    
     a1 = 0; b1 = 1; c1 = 1;
     #period;
-    
     a1 = 1; b1 = 0; c1 = 0;
     #period;
-    
     a1 = 1; b1 = 0; c1 = 1;
     #period;
-    
     a1 = 1; b1 = 1; c1 = 0;
     #period;
-    
     a1 = 1; b1 = 1; c1 = 1;
     #period;
-    
     $stop; 
 end
-
 initial
 begin
-    /* The following system task prints out the values of signals
-       every time there is a change, making debugging easier */
     $monitor("y=%b, x=%b, a=%b, b=%b, c=%b", y1, x1, a1, b1, c1);
 end
-
 endmodule
 
 
