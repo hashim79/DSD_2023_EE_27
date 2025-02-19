25855cbb6c65f65a145d57960cbff9781a4b8a5f
hashim79 <130667593+hashim79@users.noreply.github.com>
2025-02-19 17:53:31 +0500

    Add files via upload

---
+++ Lab_4/RTL/Lab_4.sv
@@ -0,0 +1,15 @@
+module Lab_4(
+     input logic a1,                                                             
+     input logic a0,                                                             
+     input logic b1,                                                             
+     input logic b0,                                                             
+     output logic r,                                                             
+     output logic g,                                                             
+     output logic b                                                              
+);                                                                              
+                                                                            
+    assign r= (~b1 & ~b0) | (a1 & a0) | (a0 & ~b1) | (a1 & ~b1) | (a1 & ~b0) ;  
+    assign g= (~a1 & ~a0 ) |(b1 & b0)| (~a1 & b0)| (~a0 & b1) | (~a1 & b1)  ;   
+    assign b= (a0 & ~b0) | (~a1 & b1) | (a1 & ~b1) | (~a0 & b0);      
+    
+endmodule
